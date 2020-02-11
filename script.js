273963d960d791c2ed7a0bc87ff7b78ddc82b5fc
Bulwson <miki2000.63@gmail.com>
2020-02-11 22:03:25 +0100

    Kuba fixes

--- script.js
+++ script.js
@@ -2,41 +2,31 @@ const mapurl = "https://europe-west1-pracabezgranic-backend.cloudfunctions.net/w
 const weatherurl = "https://europe-west1-pracabezgranic-backend.cloudfunctions.net/weather"
 
 const makeUrlGoogle = () => {
-    const addressfromuser = document.getElementById("address").value.split(" ")
-    let googleurl = mapurl
-    addressfromuser.forEach(word => {
-        googleurl += word + "+"
-    })
-    return googleurl
+    let addressFromUser = document.getElementById("address").value.split(" ")
+    let googleURL = addressFromUser.join('+')
+    return mapurl + googleURL
 }
 
-const getJsonFromUrl = async (url) => {
-    let fetched = await fetch(url)
-    let jsoned = await fetched.json()
-    return jsoned
-}
+const getJsonFromUrl = async (url) =>
+    fetch(url).then(data => data.json())
 
-const makeUrlWeather = (lat, lng) => {
-    return weatherurl + "/" + lat + "/" + lng
-}
+const makeUrlWeather = (lat, lng) =>
+    `${weatherurl}/${lat}/${lng}`
 
-const fahrenheitToCelsius = (temperature) => (temperature - 32) * 5 / 9
-
+const fahrenheitToCelsius = (temperature) =>
+    (temperature - 32) * 5 / 9
 
 const doStuff = async () => {
-    const mapapi = await getJsonFromUrl(makeUrlGoogle())
-    const lat = mapapi.results[0].geometry.location.lat
-    const lng = mapapi.results[0].geometry.location.lng
-    const weatherapi = await getJsonFromUrl(makeUrlWeather(lat, lng))
-    const apparentTemperature = Math.round(fahrenheitToCelsius(weatherapi.currently.apparentTemperature) * 10) / 10
-    const temperature = Math.round(fahrenheitToCelsius(weatherapi.currently.temperature) * 10) / 10
-    const dailyweather = weatherapi.hourly.summary
-    const summaryweather = weatherapi.currently.summary
-    document.getElementById("place").innerHTML = mapapi.results[0].formatted_address
-    document.getElementById("aboutweather").innerHTML = `It's ${temperature}℃ but it feels like ${apparentTemperature}℃.${dailyweather}${summaryweather}`
+    let mapData = await getJsonFromUrl(makeUrlGoogle())
+    let { lat, lng } = mapData.results[0].geometry.location
+    let weatherData = await getJsonFromUrl(makeUrlWeather(lat, lng))
+    let apparentTemperature = Math.round(fahrenheitToCelsius(weatherData.currently.apparentTemperature) * 10) / 10
+    let temperature = Math.round(fahrenheitToCelsius(weatherData.currently.temperature) * 10) / 10
+    let dailyWeather = weatherData.hourly.summary
+    let summaryWeather = weatherData.currently.summary
+    document.getElementById("place").innerHTML = mapData.results[0].formatted_address
+    document.getElementById("aboutweather").innerHTML = `It's ${temperature}℃ but it feels like ${apparentTemperature}℃.${dailyWeather}${summaryWeather}`
 }
-
-
 
 document.addEventListener('keydown', event => {
     if (event.keyCode == 13) doStuff()
